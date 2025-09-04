29c64d34ecbe4ada3dd797a9b618a84901d12e3b
sharin joseph <sharinjoseph37@gmail.com>
2025-09-04 19:44:28 +0000

    change the header into glassmorphism

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -22,7 +22,7 @@ export function MainNav() {
   const [isSheetOpen, setIsSheetOpen] = React.useState(false);
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/60 backdrop-blur-sm">
       <div className="container flex h-16 items-center">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
