f12c3e8b867c248e509d98aa34d251b7682d438f
sharin joseph <sharinjoseph37@gmail.com>
2025-09-08 19:33:59 +0000

    Try fixing this error: `Console Error: In HTML, <html> cannot be a child

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -125,19 +125,9 @@ export default function AdminRootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en" className="dark">
-            <head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;700&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;500;700;800&display=swap" rel="stylesheet" />
-            </head>
-            <body className={cn('min-h-screen bg-background font-body antialiased')}>
-                <AdminProvider>
-                    <AdminLayoutContent>{children}</AdminLayoutContent>
-                    <Toaster />
-                </AdminProvider>
-            </body>
-        </html>
+        <AdminProvider>
+            <AdminLayoutContent>{children}</AdminLayoutContent>
+            <Toaster />
+        </AdminProvider>
     )
 }
