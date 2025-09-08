72e7462cc8172c17da7c4a2d2b3b11cc82a44640
sharin joseph <sharinjoseph37@gmail.com>
2025-09-08 20:00:39 +0000

    remove this  (_for element span_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,6 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center gap-2', className)}>
       <Image src="/logo.png" alt="CyberHive Logo" width={56} height={56} />
-      <span className="font-headline text-3xl font-bold text-foreground">CyberHive</span>
     </div>
   );
 }
