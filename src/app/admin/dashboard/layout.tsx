c548491ccc5368455bc820e684a81e74bf4044b6
Aditya Sahu <adityasahu2703@gmail.com>
2025-09-21 22:01:17 +0000

    only authenticate at starting when admin login after this admin can expl

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -23,7 +23,9 @@ export default function AdminDashboardLayout({
     if (isCheckingAdminStatus || !isAdmin) {
         return (
             <div className="flex h-screen items-center justify-center">
-                <Loader2 className="h-8 w-8 animate-spin" />
+                <div className="flex h-full w-full flex-col items-center justify-center">
+                    <Loader2 className="h-8 w-8 animate-spin" />
+                </div>
             </div>
         );
     }
