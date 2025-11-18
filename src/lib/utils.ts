379fd3ff50d488548cbd02df420ae7faae4a4869
Aditya Sahu <adityasahu2703@gmail.com>
2025-11-18 10:39:10 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,20 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function transformGoogleDriveUrl(url: string): string {
-  if (url && url.includes('drive.google.com/file/d/')) {
-    const fileId = url.split('/d/')[1].split('/')[0];
-    return `https://drive.google.com/uc?export=view&id=${fileId}`;
+  if (typeof url !== 'string') return '';
+  
+  try {
+    const urlObject = new URL(url);
+    if (urlObject.hostname === 'drive.google.com' && urlObject.pathname.includes('/file/d/')) {
+      const fileId = url.split('/d/')[1].split('/')[0];
+      if (fileId) {
+        return `https://drive.google.com/uc?export=view&id=${fileId}`;
+      }
+    }
+  } catch (error) {
+    // Invalid URL, just return the original string
+    return url;
   }
+  
   return url;
 }
